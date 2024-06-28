8fd6cfc2177cba1f893d30a65a8e9d6833a0a8dd
bassamtiano <bassamtiano@gmail.com>
2024-06-28 17:59:37 +0900

    Adding Parser

--- run.py
+++ run.py
@@ -5,18 +5,48 @@ import lightning as L
 from model.hoax_detection_model import HoaxDetectionModel
 from utils.preprocessor import Preprocessor
 
+import argparse
+
+def input_parser():
+    parser = argparse.ArgumentParser()
+        
+    parser.add_argument("--use_gpu", action = "store_true")
+    parser.add_argument("--max_epoch", type = int, default = 10)
+    parser.add_argument("--batch_size", type = int, default = 10)
+    parser.add_argument("--model_id", type = str, default = "indolem/indobert-base-uncased")
+    parser.add_argument("--root_dir", type = str, default = "logs/indobert")
+    
+    args = parser.parse_args()
+    
+    if args.use_gpu:
+        device = "gpu"
+    else:
+        device = "cpu"
+        
+    config = {
+        "use_gpu": device,
+        "max_epoch": args.max_epoch,
+        "batch_size": args.batch_size,
+        "model_id": args.model_id,
+        "root_dir": args.root_dir
+    }
+    
+    return config
+
 if __name__ == "__main__":
     
+    config = input_parser()
+    
     dm = Preprocessor(
-        batch_size = 10
+        batch_size = config["batch_size"]
     )
     
-    model = HoaxDetectionModel(model_id = "indolem/indobert-base-uncased")
+    model = HoaxDetectionModel(model_id = config["model_id"])
     
     trainer = L.Trainer(
-        accelerator = 'cpu',
-        max_epochs = 1,
-        default_root_dir = 'logs/indobert'
+        accelerator = config["use_gpu"],
+        max_epochs = config["max_epoch"],
+        default_root_dir = config["root_dir"]
     )
     
     # Ini bagian training 
